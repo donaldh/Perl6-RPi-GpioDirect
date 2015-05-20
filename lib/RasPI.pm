000fd57076ab2fbc8456ff20a13efb6a7b4f00f9
donaldh <donald@sealgair.com>
2015-05-20 11:09:05 +0000

    Start creating a RasPI class

--- lib/RasPI.pm
+++ lib/RasPI.pm
@@ -1,6 +1,11 @@
-#
+use v6;
+
 # void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 # int munmap(void *addr, size_t length);
+
+unit class RasPI;
+
+has $!gpio;
 
 use NativeCall;
 
@@ -25,9 +30,33 @@ enum MAP_FLAGS (
   }
 );
 
-sub my-open(Str $name, int $flags) returns int is native is symbol('open') is export {*}
+my Int $BLOCK_SIZE = 4 * 1024;
+my Int $BCM2708_PERI_BASE = 0x3F000000;
+my Int $GPIO_BASE = $BCM2708_PERI_BASE + 0x00200000;
+
+sub open(Str $name, int $flags) returns int is native {*}
+sub close(int $fd) returns int is native {*}
 
 sub mmap(OpaquePointer $addr, int $length, int $prot, int $flags, int $fd, int $offset)
-  returns OpaquePointer is native is export { * } 
+  returns OpaquePointer is native { * } 
 
-sub munmap(OpaquePointer, Int) returns int is native is export { * }
+sub munmap(OpaquePointer, Int) returns int is native { * }
+
+sub perror(Str $prefix) is native { * }
+
+submethod BUILD {
+  my int $fd = open('/dev/mem', O_RDWR +| O_SYNC) or die "$!";
+  say "Opened /dev/mem with fd {$fd}";
+
+  my Pointer $null;
+  my $mem = mmap($null, $BLOCK_SIZE, PROT_READ +| PROT_WRITE, MAP_SHARED, $fd, $GPIO_BASE);
+  perror('Sorry, mmap failed') if +$mem == -1;
+  say "mmap region at {+$mem}";
+
+  close $fd;
+}
+
+method close {
+  my int $status = munmap($!gpio, $BLOCK_SIZE);
+  perror('Sorry, munmap failed') if $status == -1;
+}
--- t/raspi.t
+++ t/raspi.t
@@ -1,20 +1,6 @@
 use v6;
+
 use RasPI;
-use NativeCall;
 
-sub perror(Str $prefix) is native { * }
-
-my Int $BLOCK_SIZE = 4 * 1024;
-my Int $BCM2708_PERI_BASE = 0x3F000000;
-my Int $GPIO_BASE = $BCM2708_PERI_BASE + 0x00200000;
-
-my int $fd = my-open('/dev/mem', O_RDWR +| O_SYNC) or die "$!";
-say $fd;
-
-my Pointer $null;
-my $mem = mmap($null, $BLOCK_SIZE, PROT_READ +| PROT_WRITE, MAP_SHARED, $fd, $GPIO_BASE);
-if +$mem == -1 {
-    perror('Sorry, mmap failed');
-}
-say +$mem;
-munmap($mem, $BLOCK_SIZE);
+my $raspi = RasPI.new;
+$raspi.close;

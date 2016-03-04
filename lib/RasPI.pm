47e461229ff4326e03e3ff55d9c1c0faa2761972
Donald Hunter <donald@sealgair.com>
2016-03-04 18:22:21 +0000

    Use /dev/gpiomem and add pin details.

--- lib/RasPI.pm
+++ lib/RasPI.pm
@@ -30,6 +30,52 @@ enum MAP_FLAGS (
   }
 );
 
+my @physToGPIO = <
+  -1  -1
+   2  -1
+   3  -1
+   4  14
+  -1  15
+  17  18
+  27  -1
+  22  23
+  -1  24
+  10  -1
+   9  25
+  11   8
+  -1   7
+   0   1
+   5  -1
+   6  12
+  13  -1
+  19  16
+  26  20
+  -1  21
+>;
+
+my @physNames = <
+   3.3v     5v
+   SDA.1    5v
+   SCL.1    GND
+   GPIO.4   TxD
+   GND      RxD
+   GPIO.17  GPIO.18
+   GPIO.27  GND
+   GPIO.22  GPIO.23
+   3.3v     GPIO.24
+   MOSI     GND
+   MISO     GPIO.25
+   SCLK     CE0
+   GND      CE1
+   SDA.0    SCL.0
+   GPIO.5   GND
+   GPIO.6   GPIO.12
+   GPIO.13  GND
+   GPIO.19  GPIO.16
+   GPIO.26  GPIO.20
+   GND      GPIO.21
+>;
+
 my int32 $BLOCK_SIZE = 4 * 1024;
 my int32 $BCM2708_PERI_BASE = 0x3F000000;
 my int32 $GPIO_BASE = $BCM2708_PERI_BASE + 0x00200000;
@@ -46,16 +92,32 @@ my $errno := cglobal('libc.so.6', 'errno', int32);
 
 submethod BUILD {
 
-  my int32 $fd = open('/dev/mem', O_RDWR +| O_SYNC);
+  my int32 $fd = open('/dev/gpiomem', O_RDWR +| O_SYNC);
   die('open failed: ' ~ strerror($errno)) if $fd == -1;
-  say "Opened /dev/mem with fd {$fd}";
+  say "Opened /dev/gpiomem with fd {$fd}";
 
   my Pointer $null;
-  my $mem = mmap($null, $BLOCK_SIZE, PROT_READ +| PROT_WRITE, MAP_SHARED, $fd, $GPIO_BASE);
+  my $mem = mmap($null, $BLOCK_SIZE, PROT_READ +| PROT_WRITE, MAP_SHARED, $fd, 0);
   die('mmap failed: ' ~ strerror($errno)) if nativecast(int32, $mem) == -1;
   close $fd;
 
   $!gpio := $mem;
+}
+
+method gpio() {
+    $!gpio;
+}
+
+method read-word(int $word) {
+    $!gpio[$word];
+}
+
+method pin-name($pin) {
+    @physNames[$pin - 1];
+}
+
+method pin-gpio($pin) {
+    @physToGPIO[$pin - 1];
 }
 
 method close {
--- t/raspi.t
+++ t/raspi.t
@@ -3,4 +3,24 @@ use v6;
 use RasPI;
 
 my $raspi = RasPI.new;
+
+say 'Pin  Name      Value  Mode';
+for 1..40 -> $x {
+    #    my $b = $raspi.gpio()[$x].base(2);
+    #    say '0' x (32 - $b.chars) ~ $b;
+    my $gpio = $raspi.pin-gpio($x);
+    next if $gpio < 0;
+    say sprintf('%3i  %-8s  %5i   %03s',
+                $x, $raspi.pin-name($x), read($gpio, 13), mode($gpio).base(2)
+               );
+}
+
+sub read(Int $pin, Int $word) {
+    $raspi.read-word($word) +& (1 +< ($pin +& 31)) > 0 ?? 1 !! 0;
+}
+
+sub mode(Int $pin) {
+    $raspi.read-word($pin div 10) +> (($pin % 10) * 3) +& 7;
+}
+
 $raspi.close;

00eeed66a833c43a1d28e85efc539f1719006875
Donald Hunter <donald@sealgair.com>
2016-03-04 11:29:14 +0000

    Uprev to christmas NativeCall.

--- lib/RasPI.pm
+++ lib/RasPI.pm
@@ -30,33 +30,35 @@ enum MAP_FLAGS (
   }
 );
 
-my Int $BLOCK_SIZE = 4 * 1024;
-my Int $BCM2708_PERI_BASE = 0x3F000000;
-my Int $GPIO_BASE = $BCM2708_PERI_BASE + 0x00200000;
+my int32 $BLOCK_SIZE = 4 * 1024;
+my int32 $BCM2708_PERI_BASE = 0x3F000000;
+my int32 $GPIO_BASE = $BCM2708_PERI_BASE + 0x00200000;
 
-sub open(Str $name, int $flags) returns int is native {*}
-sub close(int $fd) returns int is native {*}
+sub open(Str $name, int32 $flags) returns int32 is native {*}
+sub close(int32 $fd) returns int32 is native {*}
 
-sub mmap(OpaquePointer $addr, int $length, int $prot, int $flags, int $fd, int $offset)
-  returns OpaquePointer is native { * } 
+sub mmap(Pointer $addr, int32 $length, int32 $prot, int32 $flags, int32 $fd, int32 $offset)
+  returns CArray[int32] is native {*}
+sub munmap(CArray[int32], int32) returns int32 is native {*}
 
-sub munmap(OpaquePointer, Int) returns int is native { * }
-
-sub perror(Str $prefix) is native { * }
+sub strerror(int32) returns Str is native {*}
+my $errno := cglobal('libc.so.6', 'errno', int32);
 
 submethod BUILD {
-  my int $fd = open('/dev/mem', O_RDWR +| O_SYNC) or die "$!";
+
+  my int32 $fd = open('/dev/mem', O_RDWR +| O_SYNC);
+  die('open failed: ' ~ strerror($errno)) if $fd == -1;
   say "Opened /dev/mem with fd {$fd}";
 
   my Pointer $null;
   my $mem = mmap($null, $BLOCK_SIZE, PROT_READ +| PROT_WRITE, MAP_SHARED, $fd, $GPIO_BASE);
-  perror('Sorry, mmap failed') if +$mem == -1;
-  say "mmap region at {+$mem}";
-
+  die('mmap failed: ' ~ strerror($errno)) if nativecast(int32, $mem) == -1;
   close $fd;
+
+  $!gpio := $mem;
 }
 
 method close {
   my int $status = munmap($!gpio, $BLOCK_SIZE);
-  perror('Sorry, munmap failed') if $status == -1;
+  die('munmap failed: ' ~ strerror($errno)) if $status == -1;
 }
